0f33bb9775a434c60e3065fadd15f556d3b8bb2e
Gsousaz <doomlighthighlord@gmail.com>
2023-08-14 15:18:16 -0300

    fix: deploy

--- src/app.js
+++ src/app.js
@@ -2,18 +2,15 @@ import express, { Router } from "express";
 import cors from "cors";
 import router from "./routers/index.router.js";
 const app = express();
-const PORT = 5000 || process.env.PORT;
-
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
 
 const teste = (req, res) => {
-    db.query("/teste")
-    console.log("está funcionando")
-}
-
-
+  db.query("/teste");
+  console.log("está funcionando");
+};
 
 app.listen(PORT, () => console.log(`O servidor está rodando na porta ${PORT}`));
